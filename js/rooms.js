2c96e8d3554070cb8ed5734004ca9c741b8229c4
Kevinvg96 <77126353+Kevinvg96@users.noreply.github.com>
2026-08-23 00:47:06 +0200

    rooms.js

--- js/rooms.js
+++ js/rooms.js
@@ -29,7 +29,7 @@ const ROOMS = [
     photos: [
       { label: "Overzicht",  file: "Living1.jpg" },
       { label: "Andere hoek", file: "Living2.jpg" },
-      { label: "Andere Nieuwe Kleur", file: "Woonkamer3.jpg" },
+      { label: "Andere Nieuwe Kleur", file: "Woonkamer3.png" },
     ],
   },
   {
